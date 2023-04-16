1482f57207240cdfbc91e92d2b4ddd7beeb2cfc1
ndirpaya <ndirpaya@gmail.com>
2023-04-16 18:25:22 +0100

    feat: sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -33,7 +33,24 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'Signed in!' };
+  async signin(dto: AuthDto) {
+    // find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Invalid credentials.');
+
+    // compare password with hash
+    const match = await argon.verify(user.hash, dto.password);
+    // if password does not match throw exception
+    if (!match) throw new ForbiddenException('Invalid credentials.');
+
+    // send back user object
+    delete user.hash;
+    return user;
   }
 }
