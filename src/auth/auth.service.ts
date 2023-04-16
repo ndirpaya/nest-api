16cac82affe15d92bdd5425babc1e5882bd2a65e
ndirpaya <ndirpaya@gmail.com>
2023-04-16 15:23:52 +0100

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials already in use. Please try again.',
+          );
+        }
+      }
+      throw error;
+    }
+  }
+
+  signin() {
+    return { msg: 'Signed in!' };
+  }
+}
